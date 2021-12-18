85653ccee7eef3d6db2ced2aa3c9bc14875835a3
klesio Nascimento <klesio.dev@gmail.com>
2021-12-18 05:19:20 -0300

    create entity from user

--- src/entities/User.ts
+++ src/entities/User.ts
@@ -1,8 +1,44 @@
-import {Entity} from "typeorm";
+import {
+  Entity,
+  PrimaryColumn,
+  Column,
+  CreateDateColumn,
+  UpdateDateColumn,
+} from "typeorm";
 
-@Entity()
+import { v4 as uuid } from "uuid";
+
+@Entity("users")
 class User {
+  @PrimaryColumn()
+  readonly id: string;
 
+  @Column()
+  name: string;
+
+  @Column()
+  email: string;
+
+  @Column()
+  admin: string;
+
+  @CreateDateColumn()
+  created_at: Date;
+
+  @UpdateDateColumn()
+  updated_at: Date;
+
+  constructor() {
+    /* 
+    se meu id vier como nulo , undfinid , vazio  
+    vair criar um id que seja igual ao uuid
+  */
+    if (!this.id) {
+      this.id = uuid();
+    }
+  }
 }
 
-export {User}
+// Entidade < - > ORM < - > (users)
+
+export { User };
